406a995b67401fdd6f6bb39334d795308eb57cbe
komaldeep1124 <kd00441@gmail.com>
2019-04-06 23:40:43 -0400

    intitial commit

---
+++ client/src/app/guards/auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CanActivate } from '@angular/router/src/utils/preactivation';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate  {
+  path: ActivatedRouteSnapshot[];
+  route: ActivatedRouteSnapshot;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) {}
+
+  canActivate(): boolean {
+    if(this.authService.loggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+
+}
---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+module.exports = {
+  //"URI": "mongodb://localhost/test",
+  //"URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson4b",
+"URI": "mongodb+srv://admin:admin@cluster0-cfxs6.azure.mongodb.net/test?retryWrites=true",
+    "secret": "SomeSecret"
+}
+
+
